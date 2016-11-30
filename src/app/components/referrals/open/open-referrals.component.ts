43ad465afdbd8d713894e2abc3eba5f478425fa0
Gary Chamberlain <gary@pro.graphics>
2016-11-30 15:34:07 -0500

    Open referrals page now filters the list by partner id and only shows those that are open

--- src/app/components/referrals/open/open-referrals.component.ts
+++ src/app/components/referrals/open/open-referrals.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable, Subscription } from 'rxjs/rx';
 import { StoreService } from '../../../store/store.service';
-import { Referral, Contact, Partner } from '../../../firebase';
+import { DataWriterService, Referral, Contact, Partner } from '../../../firebase';
 
 @Component({
   selector: 'ra-openreferrals',
@@ -10,39 +10,49 @@ import { Referral, Contact, Partner } from '../../../firebase';
 })
 export class ShowOpenReferralsComponent {
 
-  referrals$: Subscription;
-  referrals: Referral[] = [];
-  contacts: {[cid: string]: Contact} = {};
-  partners: {[pid: string]: Partner} = {};
+  to_pid = "";
 
-  constructor(private store: StoreService) {
+  partners: Observable<Partner[]>;
+
+  subscription$: Subscription;
+  referrals: Referral[] = [];
+  contactsHash: {[cid: string]: Contact} = {};
+  partnersHash: {[pid: string]: Partner} = {};
+
+  constructor(private store: StoreService, private writer: DataWriterService) {
+    this.to_pid = this.writer.pid;
   }
 
   ngOnInit() {
-    Observable.combineLatest(
-      this.store.select(store => store.referrals),
+    this.partners = this.store.select(store => store.partners);
+
+    this.subscription$ = Observable.combineLatest(
+      this.store.select(store => store.referrals).map(referrals => referrals.filter(referral => referral.status == "open")),
       this.store.select(store => store.contacts),
-      this.store.select(store => store.partners)
+      this.partners
     ).subscribe((players) => {
       if (players[0].length && players[1].length && players[2].length) {
         
-        this.contacts = players[1].reduce((hash, contact) => {
+        this.contactsHash = players[1].reduce((hash, contact) => {
           hash[contact.cid] = contact;
           return hash;
         }, {});
 
-        this.partners = players[2].reduce((hash, partner) => {
+        this.partnersHash = players[2].reduce((hash, partner) => {
           hash[partner.pid] = partner;
           return hash;
         }, {});
 
         this.referrals = players[0];
-        console.log("referrals", players, this.contacts, this.partners);
       }
     });
   }
 
   ngOnDestroy() {
-    this.referrals$.unsubscribe();
+    this.subscription$.unsubscribe();
+  }
+
+  filteredReferrals() {
+    return this.referrals.filter(referral => referral.to_pid == this.to_pid);
   }
 }
