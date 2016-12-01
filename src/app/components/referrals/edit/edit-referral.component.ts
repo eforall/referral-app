204676f1ceb1f284c6d82f5487a56ca44b8d9297
Gary Chamberlain <gary@pro.graphics>
2016-12-01 17:49:05 -0500

    Display various details on the referral page

--- src/app/components/referrals/edit/edit-referral.component.ts
+++ src/app/components/referrals/edit/edit-referral.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs/subscription';
+import { Subscription, Observable } from 'rxjs/rx';
 import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
-import { DataWriterService, ReferralDetail } from '../../../firebase';
+import { DataReaderService, DataWriterService, ContactDetail, ReferralDetail, Partner, Member } from '../../../firebase';
 import { StoreService } from '../../../store';
 
 @Component({
@@ -20,14 +20,25 @@ export class EditReferralComponent {
   controlNames = ['from_notes', 'status', 'to_uid', 'to_notes', 'five_hours',
     'result', 'jobs_created', 'jobs_preserved', 'financing_received' ];
 
-  referralDetail: ReferralDetail;
   referralDetail$: Subscription;
-  form: FormGroup;
+  partners$: Subscription;
+  contactDetail$: Subscription;
 
+  referralDetail: ReferralDetail;
+  createdDate = "";
+  contactDetail = {};
+  fromMemberName = "Unknown Member";
+  toMembers: Member[] = [];
+  fromPartner: any = { pid: "", name: "Unknown Partner" };
+  toPartner: any = { pid: "", name: "Unknown Partner" };
+
+  form: FormGroup;
+  
   constructor(private router: Router,
               private route: ActivatedRoute,
               private fb: FormBuilder,
-              private data: DataWriterService,
+              private reader: DataReaderService,
+              private writer: DataWriterService,
               private store: StoreService) {
 
     this.form = fb.group({
@@ -41,9 +52,6 @@ export class EditReferralComponent {
       jobs_preserved: '',
       financing_received: '',
     });
-
-
-
   }
 
   ngOnInit() {
@@ -55,9 +63,24 @@ export class EditReferralComponent {
     //// this.referralDetail = this.route.snapshot.data['referralDetail'];
     
     let rid = this.route.snapshot.params["rid"];
-    this.referralDetail$ = this.store.select(store => store.referralDetails[rid])
-                                    .filter(referralDetail => referralDetail !== undefined)
-                                    .subscribe((referralDetail => this.patchValues(referralDetail)));
+
+    this.referralDetail$ = Observable.combineLatest(
+      this.store.select(store => store.referralDetails[rid]).filter(referralDetail => referralDetail !== undefined),
+      this.store.select(store => store.members)
+    ).subscribe(values => {
+
+      let referralDetail: ReferralDetail = values[0];
+      let members: Member[] = values[1];
+
+      if (referralDetail !== undefined && members !== undefined) {
+        
+        this.patchValues(referralDetail);
+        let fromMember = members.find(member => member.pid == referralDetail.from_pid);
+        if (fromMember) this.fromMemberName = fromMember.displayName;
+
+        this.toMembers = members.filter(member => member.pid = referralDetail.to_pid);
+      }
+    });
   }
 
   patchValues(referralDetail: ReferralDetail) {
@@ -70,11 +93,28 @@ export class EditReferralComponent {
     });
     
     this.referralDetail = referralDetail;
-    
+    this.createdDate = new Date(referralDetail.timestamp).toDateString();
+    this.reader.loadContactDetail(referralDetail.cid);
+
+    this.contactDetail$ = this.store.select(store => store.contactDetails[referralDetail.cid])
+                              .filter(contactDetail => contactDetail !== undefined)
+                              .subscribe(contactDetail => {
+                                this.contactDetail = contactDetail;
+                              });
+
+    this.partners$ = this.store.select(store => store.partners)
+                  .subscribe(partners => {
+                      partners.forEach(partner => {
+                        if (partner.pid == referralDetail.from_pid) this.fromPartner = partner;
+                        if (partner.pid == referralDetail.to_pid) this.toPartner = partner;
+                      }); 
+                  });
   }
 
   ngOnDestroy() {
     this.referralDetail$.unsubscribe();
+    if (this.partners$) this.partners$.unsubscribe();
+    if (this.contactDetail$) this.contactDetail$.unsubscribe();
   }
 
 }
